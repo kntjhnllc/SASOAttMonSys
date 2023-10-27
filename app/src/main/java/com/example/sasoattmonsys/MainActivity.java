6e5b52eea888714238b5f4dd305607b35c58b39d
kntjhnllc <kentjohnliloc@gmail.com>
2023-10-27 13:23:11 +0800

    Fix orientation

--- app/src/main/java/com/example/sasoattmonsys/MainActivity.java
+++ app/src/main/java/com/example/sasoattmonsys/MainActivity.java
@@ -13,21 +13,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
-    private FloatingActionButton fab;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fab = findViewById(R.id.fab);
-        ScanOptions options = new ScanOptions();
-        options.setOrientationLocked(false);
-        barcodeLauncher.launch(options);
-        onButtonClick(fab);
-
     }
     // Register the launcher and result handler
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+
             result -> {
                 if(result.getContents() == null) {
                     Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
@@ -39,5 +32,8 @@ public class MainActivity extends AppCompatActivity {
     // Launch
     public void onButtonClick(View view) {
         barcodeLauncher.launch(new ScanOptions());
+        ScanOptions options = new ScanOptions();
+        options.setOrientationLocked(false);
+        barcodeLauncher.launch(options);
     }
 }
